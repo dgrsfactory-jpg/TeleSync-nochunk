680f9c02a696032831806fdacec29b9bb798c967
dgrsfactory-jpg <dgrs.factory@gmail.com>
2026-09-15 14:15:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
-# Get all sys args after file name
-args=("$@")
-
-source "$(dirname "$0")/venv/bin/activate"
-
-# Run src/main.py with sys args
+#!/data/data/com.termux/files/usr/bin/bash
 python "$(dirname "$0")/src/main.py" "$@"
